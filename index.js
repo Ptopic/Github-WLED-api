97900a31832f2cfbbbd82f162231acadf5702b3b
Ptopic <petartopic2002@gmail.com>
2024-09-05 20:04:28 +0200

    Add check build function

--- index.js
+++ index.js
@@ -29,30 +29,123 @@ app.get('/check-merge/:organization/:repo/:prId', async (req, res) => {
 						Authorization: `Bearer ${haToken}`,
 					},
 				});
-				return res
+				res
 					.status(200)
 					.json({ stop: true, message: 'PR merged, webhook triggered.' });
 			} catch (error) {
-				console.error(
-					'Error triggering Home Assistant webhook:',
-					error.message
-				);
-				return res
-					.status(500)
-					.json({
-						stop: true,
-						error: 'Failed to trigger Home Assistant webhook.',
-					});
+				res.status(500).json({
+					stop: true,
+					error: 'Failed to trigger Home Assistant webhook.',
+				});
 			}
 		}
 	} catch (error) {
 		if (error.response && error.response.status !== 404) {
-			console.error('Error checking PR merge status:', error.message);
-			return res
+			res
 				.status(500)
 				.json({ stop: true, error: 'Failed to check PR merge status.' });
 		}
-		return res.status(500).json({ stop: false, message: 'PR not merged.' });
+		res.status(500).json({ stop: false, message: 'PR not merged.' });
+	}
+});
+
+app.get('/check-comments/:organization/:repo/:prId', async (req, res) => {
+	const { organization, repo, prId } = req.params;
+	const githubToken = process.env.GITHUB_TOKEN;
+	const haToken = process.env.HA_TOKEN;
+});
+
+app.get('/check-build/:organization/:repo/:prId', async (req, res) => {
+	const { organization, repo, prId } = req.params;
+	const githubToken = process.env.GITHUB_TOKEN;
+	const haToken = process.env.HA_TOKEN;
+
+	try {
+		const response = await axios.get(
+			`https://api.github.com/repos/${organization}/${repo}/pulls/${prId}/commits`,
+			{
+				headers: {
+					Authorization: `Bearer ${githubToken}`,
+				},
+			}
+		);
+
+		const commits = response.data;
+
+		const lastCommit = commits[commits.length - 1];
+
+		const lastCommitSha = lastCommit.sha;
+
+		const commitStatusResponse = await axios.get(
+			`https://api.github.com/repos/${organization}/${repo}/commits/${lastCommitSha}/status`,
+			{
+				headers: {
+					Authorization: `Bearer ${githubToken}`,
+				},
+			}
+		);
+
+		// failure, pending, success
+		const lastCommitStatus = commitStatusResponse.data.state;
+
+		if (lastCommitStatus === 'failure') {
+			try {
+				await axios.post(webhookUrl, {
+					headers: {
+						Authorization: `Bearer ${haToken}`,
+					},
+				});
+				res.status(200).json({
+					stop: true,
+					message: 'Build failed, webhook triggered.',
+				});
+			} catch (error) {
+				res.status(500).json({
+					stop: true,
+					error: 'Failed to trigger Home Assistant webhook.',
+				});
+			}
+		} else if (lastCommitStatus === 'success') {
+			try {
+				await axios.post(webhookUrl, {
+					headers: {
+						Authorization: `Bearer ${haToken}`,
+					},
+				});
+				res.status(200).json({
+					stop: true,
+					message: 'Build success, webhook triggered.',
+				});
+			} catch (error) {
+				res.status(500).json({
+					stop: true,
+					error: 'Failed to trigger Home Assistant webhook.',
+				});
+			}
+		} else {
+			try {
+				await axios.post(webhookUrl, {
+					headers: {
+						Authorization: `Bearer ${haToken}`,
+					},
+				});
+				res.status(200).json({
+					stop: false,
+					message: 'Build pending, webhook triggered.',
+				});
+			} catch (error) {
+				res.status(500).json({
+					stop: true,
+					error: 'Failed to trigger Home Assistant webhook.',
+				});
+			}
+		}
+
+		res.status(200).json({ stop: true, message: lastCommitStatus });
+	} catch (error) {
+		res
+			.status(500)
+			.json({ stop: true, error: 'Failed to check PR merge status.' });
 	}
 });
 
